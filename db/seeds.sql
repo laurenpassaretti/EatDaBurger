966ff33a17f8019b9521f7582c2156c3a0ff013b
Lauren Doss <doss.laurene@gmail.com>
2020-04-16 18:29:50 -0700

    updated seeds and scschema sql pages and also updated connection.js to ensure it connects to the database:

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon', FALSE); 
